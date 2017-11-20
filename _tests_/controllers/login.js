d12776dd068cc658fd7aef5f7b443fe15baeae31
Dwayne Johnson <Dwayne_J23@yahoo.com>
2017-11-20 16:52:23 -0500

    Base commit

---
+++ _tests_/controllers/login.js
@@ -0,0 +1,14 @@
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      expect([1, 2, 3].indexOf(4)).toBe(-1);
+    });
+  });
+
+  describe('#push()', function() {
+    it('should add the element and increase size by 1', () => {
+      expect([1, 2, 3].indexOf(4)).toBe(-1);
+    });
+  });
+
+});
